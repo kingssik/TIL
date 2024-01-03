5a82c3aa9c94fcadc15d75e6e099849c946cfc25
kingssik <hyunsikkang25@gmail.com>
2024-01-03 17:30:08 +0900

    call back 함수

--- javascript/test.js
+++ javascript/test.js
@@ -1,17 +1,28 @@
-// 기본 매개변수 예시
-
-// 올해가 윤년인지 알려주는 함수
-const isLeapYear = function (year = new Date().getFullYear()) {
-  // 기본 매개변수 사용 불가했던 과거의 javascript 방식 3가지
-  /**
-  year = typeof(year) === 'undefined' ? new Date().getFullYear() : year
-  year = typeof(year) !== 'undefined' ? year : new Date().getFullYear() 
-  year = year || new Date().getFullYear()
-  */
-
-  console.log(`year : ${year}`)
-  return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)
+const test1 = function (callback) {
+  callback()
 }
 
-console.log(isLeapYear())
-// 자동으로 올해 연도가 매개변수 year에 들어감
+const func1 = function () {
+  console.log('hi')
+}
+
+test1(func1)
+// test(10) // 함수가 아닌 것을 매개변수로 전달 -> error 발생(a is not a function)
+
+// 콜백함수에 매개변수가 있는 경우
+const test2 = function (arr, callback) {
+  for (const value of arr) {
+    callback(value)
+  }
+}
+
+const func2 = function (a) {
+  console.log(`${a}`)
+}
+
+test2([11,22,33,44,55], func2)
+/* 이렇게도 작성 가능
+test2([11,22,33,44,55], function (a) {
+  console.log(`${a}`)
+})
+*/
