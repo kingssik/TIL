4d2c5cd55f851033af4fce1bc0157e5ce045ade5
kingssik <hyunsikkang25@gmail.com>
2024-01-04 16:21:04 +0900

    타이머 함수, 타이머 제거 함수, 즉시 호출 함수

--- javascript/test.js
+++ javascript/test.js
@@ -1,55 +1,28 @@
-const test1 = function (callback) {
-  callback()
-}
+// 타이머 함수
+const a = setTimeout(function () {
+  console.log('setTimeout 함수')
+}, 1000)
 
-const func1 = function () {
-  console.log('hi')
-}
+const b = setInterval(function () {
+  console.log('setInterval 함수')
+}, 1000)
 
-test1(func1)
-// test(10) // 함수가 아닌 것을 매개변수로 전달 -> error 발생(a is not a function)
+console.log(a, b)
 
-// 콜백함수에 매개변수가 있는 경우
-const test2 = function (arr, callback) {
-  for (const value of arr) {
-    callback(value)
-  }
-}
+// 타이머 제거 함수
+clearTimeout(a)
+clearInterval(b)
 
-const func2 = function (a) {
-  console.log(`${a}`)
-}
+// 즉시 호출 함수
+(function () {
+  
+})()
 
-test2([11,22,33,44,55], func2)
-/* 이렇게도 작성 가능
-test2([11,22,33,44,55], function (a) {
-  console.log(`${a}`)
-})
-*/
+// 아래와 동일함
+// const f = function () {}
+// f()
+// function () {} 대신 f를 썼을 뿐임
 
-// 콜백함수 예제
-// 배열의 콜백함수를 사용하는 메서드
+(() => {
 
-// 1. forEach() -> 배열의 요소를 콜백함수로 전달해서 반복하는 함수
-const arr1 = [11,22,33,44,55,66]
-arr1.forEach(function (value, index) {
-  console.log(`${index + 1}번째 값 : ${value}`)
-})
-
-// 2. filter() -> 특정 기준으로 filtering 한 배열 리턴
-let arr2 = [11,22,33,44,55,66]
-arr2 = arr2.filter(function (value) {
-  return value % 2 === 0 // 짝수인 수만 filtering
-})
-// 화살표 함수로 나타내면
-arr2 = arr2.filter((value) => value % 2 === 0)
-console.log(arr2)
-
-// 3. map() -> 기존의 배열 요소를 기반으로 새로운 배열을 만들어서 리턴
-let arr3 = [11,22,33,44,55,66]
-arr3 = arr3.map(function (value) {
-  return value + "##"
-})
-// 화살표 함수로 나타내면
-arr3 = arr3.map((value) => value + "##")
-console.log(arr3)
+})()
