4c4495d6e1a7b4dd26d0b2616a8003006c864e50
kingssik <hyunsikkang25@gmail.com>
2024-01-05 23:21:24 +0900

    객체에 동적으로 속성 추가/제거 하기

---
+++ javascript/quiz1.js
@@ -0,0 +1,11 @@
+const book = {
+  name: 'java script',
+  price: 18000,
+}
+
+console.log(book)
+console.log(book.name)
+console.log(book.price)
+
+book.publisher = 'nobody'
+console.log(book.publisher)
--- javascript/test.js
+++ javascript/test.js
@@ -1,34 +1,13 @@
-// 배열
-const arr = [100, 20, 'String', true, function () {}, () => {}]
-console.log(arr[0])
-console.log(arr[1])
-arr[1] = 300
-console.log(arr[1])
-
-// 객체
-const object = {
-  name: 'hong',
-  age: 7
+// 객체의 키와 값을 정적으로 생성
+const pet = {
+  name: 'king',
+  age: 8
 }
-console.log(object.name)
-object.name = 'king'  // 객체 내부 변수의 값 변경
-console.log(object.age)
-object.age = 10       // 객체 내부 변수의 값 변경
-console.log(object.age)
 
-// 속성과 메서드
-const dog = {
-  name: 'jim',
-  age: 3,
-  bark: function () {
-    console.log(`${this.name} 짖는다`)
-    console.log(`${dog.name} 짖는다`)
-    // console.log(this) // dog 객체 출력
-  },
-  sleep: () => {
-    console.log(`${dog.name} 잔다`)
-    // console.log(this) // window 객체 출력
-  }
-}
-dog.bark()
-dog.sleep()
+// 객체의 키와 값을 동적으로 생성
+pet.color = 'grey'
+console.log(pet)  // { name: 'king', age: 8, color: 'grey' }
+
+// 객체의 키와 값을 동적으로 제거
+delete pet.color
+console.log(pet)  // { name: 'king', age: 8}
