6c246c92dd33ca70219e556e4dca4be92e816aef
kingssik <hyunsikkang25@gmail.com>
2024-01-25 17:05:02 +0900

    상속

--- javascript/test.js
+++ javascript/test.js
@@ -1,13 +1,24 @@
-// private 메서드
-class Square {
-  #test () {
-    console.log('private 메서드임')
+class Rectangle {
+  constructor (width, height) {
+    this.width = width
+    this.height = height
   }
-  
-  constructor () {
-    this.#test()
+
+  getArea () {return this.width * this.height}
+  getPerimeter () {return 2 * (this.width + this.height)}
+}
+
+// 상속 : Rectangle(부모 클래스), Square(자식 클래스)
+class Square extends Rectangle {
+  constructor (width) {
+    super(width, width) // 부모의 생성자로 자식의 생성자에서만 호출하면 됨
   }
 }
 
-const square = new Square()
-// square.#test() // 오류 발생
+const rect = new Rectangle(20, 10)
+console.log(`넓이: ${rect.getArea()}`)
+console.log(`둘레: ${rect.getPerimeter()}`)
+
+const square = new Square(10)
+console.log(`넓이: ${square.getArea()}`)
+console.log(`둘레: ${square.getPerimeter()}`)
